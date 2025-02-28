46a25d5ec2d0b94db8b1d3427ea46c3e72d2ef04
Ghulam Nabi Ahmed <ghulam@sharedstudios.com>
2025-02-28 02:03:41 -0500

    BUG fixed

--- frontend/fare-evaders/src/components/Navbar.js
+++ frontend/fare-evaders/src/components/Navbar.js
@@ -6,8 +6,9 @@ function Navbar() {
     <nav style={{ padding: "10px", backgroundColor: "#333", color: "white" }}>
       <h2>MTA Subway Data</h2>
       <Link to="/" style={{ marginRight: "20px", color: "white" }}>Home</Link>
-      <Link to="/subway" style={{ color: "white" }}>Subway Data</Link>
-      <Link to="/alerts" style={{ color: "white", marginLeftt: "20px" }}>Alerts</Link>
+      <Link to="/subway" style={{ marginRight: "20px", color: "white" }}>Subway Data</Link>
+      <Link to="/alerts" style={{ marginRight: "20px", color: "white" }}>Alerts</Link>
+      <Link to="/nqrw" style={{ color: "white" }}>NQRW Data</Link> {/* Added NQRW link */}
     </nav>
   );
 }
