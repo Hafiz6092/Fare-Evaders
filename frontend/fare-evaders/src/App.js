b96fc61ed610ff30249e08bad9f5402dd6edd509
Ghulam Nabi Ahmed <Ghulamnabia75@gmail.com>
2025-03-03 14:45:28 -0500

    moving towards the creation of map

--- frontend/fare-evaders/src/App.js
+++ frontend/fare-evaders/src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";  // Import Navbar
 import Home from "./pages/Home";
-import SubwayData from "./pages/SubwayData";
+// import SubwayData from "./pages/SubwayData";
 import SubwayAlerts from "./pages/SubwayAlerts";
-import NQRWSubwayData from "./pages/NQRWSubwayData";  // Import NQRW Component
+// import NQRWSubwayData from "./pages/NQRWSubwayData";  // Import NQRW Component
 
 function App() {
     return (
@@ -11,9 +11,9 @@ function App() {
             <Navbar />  {/* Add Navbar here */}
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/subway" element={<SubwayData />} />
+                {/* <Route path="/subway" element={<SubwayData />} /> */}
                 <Route path="/alerts" element={<SubwayAlerts />} />
-                <Route path="/nqrw" element={<NQRWSubwayData />} /> {/* New route */}
+                {/* <Route path="/nqrw" element={<NQRWSubwayData />} /> New route */}
             </Routes>
         </Router>
     );
--- frontend/fare-evaders/src/components/Navbar.js
+++ frontend/fare-evaders/src/components/Navbar.js
@@ -6,9 +6,9 @@ function Navbar() {
     <nav style={{ padding: "10px", backgroundColor: "#333", color: "white" }}>
       <h2>MTA Subway Data</h2>
       <Link to="/" style={{ marginRight: "20px", color: "white" }}>Home</Link>
-      <Link to="/subway" style={{ marginRight: "20px", color: "white" }}>Subway Data</Link>
+      {/* <Link to="/subway" style={{ marginRight: "20px", color: "white" }}>Subway Data</Link> */}
       <Link to="/alerts" style={{ marginRight: "20px", color: "white" }}>Alerts</Link>
-      <Link to="/nqrw" style={{ color: "white" }}>NQRW Data</Link> {/* Added NQRW link */}
+      {/* <Link to="/nqrw" style={{ color: "white" }}>NQRW Data</Link> Added NQRW link */}
     </nav>
   );
 }
