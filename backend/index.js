861c2a63a94e0930cfe90aba5b60e55a19a5f4d5
Ghulam Nabi Ahmed <Ghulamnabia75@gmail.com>
2025-03-03 14:44:46 -0500

    subwayall and subwayNQRW being paused

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const subwayAllRoutes = require('./routes/subwayAll'); // Fetch all subway data
-const subwayNQRWRoutes = require('./routes/subwayNQRW'); // Fetch only NQRW data
+// const subwayAllRoutes = require('./routes/subwayAll'); // Fetch all subway data
+// const subwayNQRWRoutes = require('./routes/subwayNQRW'); // Fetch only NQRW data
 const subwayAlertsRoutes = require('./routes/subwayAlerts');
 
 
@@ -13,8 +13,8 @@ const PORT = process.env.PORT || 5050;
 app.use(cors());
 
 // **Use the separated routes**
-app.use('/api/mta', subwayAllRoutes);
-app.use('/api/mta', subwayNQRWRoutes);
+// app.use('/api/mta', subwayAllRoutes);
+// app.use('/api/mta', subwayNQRWRoutes);
 app.use('/api/mta', subwayAlertsRoutes);
 
 app.listen(PORT, () => {
