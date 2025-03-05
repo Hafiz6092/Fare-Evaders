6e33660fd6eac9ee7e8b2df1aab1c9b00d81e8c8
Ghulam Nabi Ahmed <Ghulamnabia75@gmail.com>
2025-03-04 19:04:13 -0500

    map's max zoom out is set to only New York City

--- frontend/fare-evaders/src/components/MapComponent.js
+++ frontend/fare-evaders/src/components/MapComponent.js
@@ -2,19 +2,29 @@ import React from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-const MapComponent = () => {
+function MapComponent() {
+  // Define the boundaries for NYC (to prevent panning outside)
+  const nycBounds = [
+    [40.4774, -74.2591], // Southwest corner (bottom-left: Staten Island)
+    [40.9176, -73.7004], // Northeast corner (top-right: The Bronx)
+  ];
+
   return (
-    <MapContainer 
-      center={[40.7128, -74.0060]} 
-      zoom={11} 
-      style={{ height: "800px", width: "100%" }}
+    <MapContainer
+      center={[40.7128, -74.006]} // NYC center coordinates
+      zoom={12} // Default zoom level
+      minZoom={11} // Prevent zooming too far out (keeps it around NYC)
+      maxZoom={18} // Allows zooming in for details
+      maxBounds={nycBounds} // Restrict panning to only NYC
+      maxBoundsViscosity={1.0} // Ensures users can't drag outside NYC
+      style={{ height: "100vh", width: "100%" }}
     >
-      {/* Default Tile Layer */}
-      <TileLayer 
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
     </MapContainer>
   );
-};
+}
 
 export default MapComponent;
