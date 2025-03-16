ca2df0cedcdbbc1bdd984d57724ccfdf0206e59e
Mohammad Hussain <144273111+mhussain214@users.noreply.github.com>
2025-03-16 15:23:26 -0400

    create link for bar graph

---
+++ frontend/fare-evaders/src/pages/Bar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import BarGraph from "../components/BarGraph";
+
+function Bar() {
+    return (
+    <>
+    <BarGraph />  {/* Map will be displayed here */}
+    </>
+        
+     
+    );
+  }
+  
+  export default Bar;
+  
