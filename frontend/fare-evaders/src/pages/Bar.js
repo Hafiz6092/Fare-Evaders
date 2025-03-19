42a3b956e30252cce780639c4321fd0c07ec8300
Ghulam Nabi Ahmed <Ghulamnabia75@gmail.com>
2025-03-18 20:29:06 -0400

    minor fixes

--- frontend/fare-evaders/src/pages/Bar.js
+++ frontend/fare-evaders/src/pages/Bar.js
@@ -3,7 +3,7 @@ import BarGraph from "../components/BarGraph";
 
 function Bar() {
     return (
-      <div class="bg-gray-950" style={{ padding: "20px" }}>
+      <div class="bg-gray-950" style={{ padding: "70px" }}>
       <BarGraph />  {/* Map will be displayed here */}
     </div>
     );
